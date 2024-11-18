ec2b4c4f8e5e2de2cf1b40d92582cc2fde36e71b
V1m4v0 <almax.crazy2015@yandex.ru>
2024-11-18 08:44:26 +0300

    Add files via upload

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+def func_template(request):
+    return render(request, 'second_template/func_template.html')
+
+class class_template(TemplateView):
+    template_name = 'second_template/class_template'
